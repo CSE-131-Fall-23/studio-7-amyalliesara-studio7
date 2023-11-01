f9185814326200aef3ea2a8c33ef98f5e88014c4
sara.moss <sara.moss@10.232.141.148>
2023-11-01 13:46:50 -0500

    Die

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,5 +1,23 @@
 package studio7;
 
 public class Die {
-
+	private int sides;
+	
+	public Die(int sides) 
+	{
+		super();
+		this.sides = sides;
+	}
+	
+	public int roll()
+	{
+		return (int)(Math.random()*this.sides+1);
+	}
+	
+	public static void main(String[] args)
+	{
+		Die die = new Die(20);
+		System.out.print(die.roll());
+	}
+	
 }
---
+++ src/studio7/Fraction.java
@@ -0,0 +1,12 @@
+package studio7;
+
+public class Fraction {
+	private int numerator;
+	private int denominator;
+	
+	public Fraction(int initNum, int initDen)
+	{
+		numerator = initNum;
+		denominator = initDen;
+	}
+}
